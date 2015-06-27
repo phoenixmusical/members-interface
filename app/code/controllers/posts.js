e91a2a7c7c083c3d31bb9364b755aff975b0e4c3
Frederic Gascon <frederic.gascon@gmail.com>
2015-06-27 19:08:05 -0400

    add post

--- app/code/controllers/posts.js
+++ app/code/controllers/posts.js
@@ -7,10 +7,27 @@ function PostsCtrl($scope, $routeParams, api){
 			$scope.comity = comity;
 		});
 	
+	$scope.posts = [];
 	api.get('/comities/'+$routeParams.id+'/posts')
 		.then(function(posts){
 			$scope.posts = posts;
 		});
+	
+	$scope.addInProgress = false;
+	$scope.newPost = {
+		name: ""
+	};
+	$scope.saveNewPost = function(){
+		$scope.addInProgress = true;
+		api.post('/comities/'+$routeParams.id+'/posts', $scope.newPost).then(function(post){
+			$scope.newPost = {};
+			$scope.posts.push(post);
+			$scope.addInProgress = false;
+		}, function(err){
+			console.error(err);
+			$scope.errorMessage = "Une erreur est survenue lors de la sauvegarde";
+		});
+	};
 }
 
 module.exports = PostsCtrl;
